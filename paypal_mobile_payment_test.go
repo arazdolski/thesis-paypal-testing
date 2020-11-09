545a8eea9b975a99e2c8737930130622b65cd854
Aleksandr <alrazd@ttu.ee>
2020-11-09 20:49:51 +0200

    Fix suite for tests

--- paypal_mobile_payment_test.go
+++ paypal_mobile_payment_test.go
@@ -1,66 +1,37 @@
 package automation
 
 import (
-	"os"
 	"time"
-
-	"github.com/juju/errors"
-	"github.com/subosito/gotenv"
 )
 
-func (suite *AutomationTestSuite) TestMobilePayment() error {
-
-	gotenv.Load()
-
-	if err := suite.page.Find("#ul-btn").Click(); err != nil {
-		return errors.Annotate(err, "clicking login button failed")
-	}
-
-	if err := suite.page.Find("input[name='login_email']").Fill(os.Getenv("PAYPAL_USER_EMAIL")); err != nil {
-		return errors.Annotate(err, "filling email field failed")
-	}
-
-	if err := suite.page.Find("#btnNext").Click(); err != nil {
-		return errors.Annotate(err, "clicking Next button failed")
-	}
-
-	time.Sleep(time.Second)
-
-	if err := suite.page.Find("input[name='login_password']").Fill(os.Getenv("PAYPAL_USER_PASSWORD")); err != nil {
-		return errors.Annotate(err, "filling password field failed")
-	}
-
-	if err := suite.page.Find("#btnLogin").Click(); err != nil {
-		return errors.Annotate(err, "clicking Next button failed")
-	}
+func (suite *AutomationTestSuite) TestMobilePayment() {
+	suite.LoginToAccount()
 
 	if err := suite.page.Find("#header-transfer").Click(); err != nil {
-		return errors.Annotate(err, "clicking Next button failed")
+		suite.Require().NoError(err)
 	}
 
 	time.Sleep(time.Second)
 
 	if err := suite.page.Find("input[name='autocomplete-input']").Fill("+15417543010"); err != nil {
-		return errors.Annotate(err, "clicking Next button failed")
+		suite.Require().NoError(err)
 	}
 
 	time.Sleep(time.Second * 3)
 
 	if err := suite.page.Find("#downshift-0-item-0 > div").Click(); err != nil {
-		return errors.Annotate(err, "clicking on Send to failed")
+		suite.Require().NoError(err)
 	}
 
 	time.Sleep(time.Second * 3)
 
 	if err := suite.page.Find("#fn-amount").Click(); err != nil {
-		return errors.Annotate(err, "clicking Next button failed")
+		suite.Require().NoError(err)
 	}
 
 	if err := suite.page.Find("#fn-amount").SendKeys("1"); err != nil {
-		return errors.Annotate(err, "clicking Next button failed")
+		suite.Require().NoError(err)
 	}
 
 	suite.page.CloseWindow()
-
-	return nil
 }
--- suite_test.go
+++ suite_test.go
@@ -3,6 +3,7 @@ package automation
 import (
 	"os"
 	"testing"
+	"time"
 
 	"github.com/subosito/gotenv"
 
@@ -54,4 +55,30 @@ func (suite *AutomationTestSuite) SetupSuite() {
 
 func TestSuite(t *testing.T) {
 	suite.Run(t, new(AutomationTestSuite))
+}
+
+// private
+
+func (suite *AutomationTestSuite) LoginToAccount() {
+	if err := suite.page.Find("#ul-btn").Click(); err != nil {
+		suite.Require().NoError(err)
+	}
+	
+	if err := suite.page.Find("input[name='login_email']").Fill(os.Getenv("PAYPAL_USER_EMAIL")); err != nil {
+		suite.Require().NoError(err)
+	}
+
+	if err := suite.page.Find("#btnNext").Click(); err != nil {
+		suite.Require().NoError(err)
+	}
+
+	time.Sleep(time.Second)
+
+	if err := suite.page.Find("input[name='login_password']").Fill(os.Getenv("PAYPAL_USER_PASSWORD")); err != nil {
+		suite.Require().NoError(err)
+	}
+
+	if err := suite.page.Find("#btnLogin").Click(); err != nil {
+		suite.Require().NoError(err)
+	}
 }
