40639ab2dd6dc2f2851f26d119d0520aff637e60
Aleksandr <alrazd@ttu.ee>
2020-11-10 15:57:50 +0200

    Delete Headless chrome option

--- suite_test.go
+++ suite_test.go
@@ -23,18 +23,7 @@ func (suite *AutomationTestSuite) SetupSuite() {
 
 	gotenv.Load()
 
-	suite.Driver = agouti.ChromeDriver(
-		agouti.ChromeOptions("args", []string{
-			// "--headless",
-			"--allow-insecure-localhost",
-			"--disable-gpu",
-			"--homepage=about:blank",
-			"--no-first-run",
-			"--no-default-browser-check",
-			"--no-sandbox",
-		}),
-		agouti.Debug,
-	)
+	suite.Driver = agouti.ChromeDriver()
 
 	if err := suite.Driver.Start(); err != nil {
 		suite.Require().NoError(err)
